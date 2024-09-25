d0dfc900714eb1469f42c7e4bd749ee4eb6674b2
tbs01215 <tbs01215@gmail.com>
2024-09-25 12:04:37 +0900

    done

---
+++ 1.js
@@ -0,0 +1,15 @@
+// x = new Number()를 해두면 에디터에서 메소드 자동완성이 되는게 편해서 js 함수에서 종종 이용했습니다.
+function solution(x = new Number(), n = new Number()) {
+  let answer = [];
+
+  // "ctrl + F" 에 용이해 i 보다 ii를 선호합니다.
+  for (let ii = 1; ii < n + 1; ii++) {
+    answer.push(x * ii);
+  }
+
+  return answer;
+}
+
+console.log(solution(2, 5)); // [ 2, 4, 6, 8, 10 ]
+console.log(solution(4, 3)); // [ 4, 8, 12 ]
+console.log(solution(-4, 2)); // [ -4, -8 ]
---
+++ 2.js
@@ -0,0 +1,11 @@
+function solution(phoneNumber = new String()) {
+  let answer = "";
+
+  const pnLen = phoneNumber.length;
+  answer = "*".repeat(pnLen - 4) + phoneNumber.slice(pnLen - 4, pnLen);
+
+  return answer;
+}
+
+console.log(solution("01033334444")); // *******4444
+console.log(solution("027778888")); // *****8888
